8fe9b7860c0c69079d7a6a9b16d561c70b8f00fb
Malcolm Tan <mlclmtan@gmail.com>
2024-04-01 04:28:13 +0800

    feat: Add and integrate vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          {children}
+          <Analytics />
+        </AntdRegistry>
       </body>
     </html>
   );
